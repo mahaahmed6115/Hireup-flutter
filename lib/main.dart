51b2b1188304ab98dd2d8baebbf09e4675d51392
noha <mokam0251@gmail.com>
2026-01-30 09:27:05 -0800

    edit main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hireup/onbording_screens/splashscreen.dart';
-
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -8,9 +8,20 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: Splashscreen());
+    return ScreenUtilInit(
+      designSize: const Size(440, 956),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (_ , child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          home: Splashscreen(),
+        );
+      },
+
+    );
   }
-}
+  }
+
